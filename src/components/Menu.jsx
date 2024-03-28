7819aaf7d56eef42a3f84e8c13c3ccca59b9bca5
justin etienne <juetienne7@gmail.com>
2024-03-28 12:50:06 +0900

    menu vertical modified to work on safari, small adjustment on menu right position (not perfect)

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,7 @@ export const Menu = (props) => {
   return (
     <>
       <div
-        className={`z-10 fixed top-0 right-1 md:right-4 bottom-0 overflow-hidden flex flex-col`}
+        className={`z-10 fixed top-0 right-1 md:right-3 bottom-0 overflow-hidden flex flex-col`}
       >
         <div className="flex-1 flex items-center justify-start flex-col gap-6 p-2 w-8 md:w-12">
           <MenuButton label="About" onClick={() => onSectionChange(0)} />
@@ -20,11 +20,11 @@ export const Menu = (props) => {
 const MenuButton = (props) => {
   const { label, onClick } = props;
   return (
-    <button
+    <a
       onClick={onClick}
       className="text-2xl font-bold cursor-pointer text-sky-800 hover:text-sky-600 transition-colors duration-200 text-vertical"
     >
       {label}
-    </button>
+    </a>
   );
 };
